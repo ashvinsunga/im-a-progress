631c423f975b3da344cd603f5850aad2c4de381a
Ashvin <ashvinsunga@yahoo.com>
2025-02-05 17:03:50 +0800

    Designed the intro

--- im-a-progress/src/routes/AllRoutes.jsx
+++ im-a-progress/src/routes/AllRoutes.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom"
-import { Home, Projects } from "../pages"
+import { Home, About, Projects } from "../pages"
 
 export const AllRoutes = () => {
   return (
     <>
         <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/projects" element={<Projects/>}/>
+            <Route path="" element={<Home/>}/>
+            <Route path="about" element={<About />}/>
+            <Route path="projects" element={<Projects/>}/>
+            
         </Routes>
     </>
   )
