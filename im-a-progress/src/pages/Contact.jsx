5fa95c0e4ad33fead6432741ded091cd16315717
Ashvin <ashvinsunga@yahoo.com>
2025-02-06 00:03:28 +0800

    Added about and project section

---
+++ im-a-progress/src/pages/Contact.jsx
@@ -0,0 +1,19 @@
+
+export default function Contact() {
+  return (
+    <section>
+        <p>Get in touch</p>
+        <p>Don&apos;t be shy, say hello.
+        </p>
+        <p>
+            Thank you for visiting! Whether you have a question, comment, or simply want to say hello, I encourage you to reach out. I&apos;m here to assist and love connecting with my community, so don&apos;t hesitate to get in touch.
+        </p>
+        <p>
+            +63 976 027 9150
+        </p>
+        <p>
+            ashvinsunga@yahoo.com.ph
+        </p>
+    </section>
+  )
+}
--- im-a-progress/src/pages/index.jsx
+++ im-a-progress/src/pages/index.jsx
@@ -1,3 +1,4 @@
 export { About } from "./About";
 export { Home } from "./Home";
 export { Projects } from "./Projects"
+export { Contact } from "./Contact";
